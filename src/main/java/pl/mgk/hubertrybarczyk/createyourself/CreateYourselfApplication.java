fd08bb921af3adfaaa2fa0a4e6038464304403ce
Number23 <vert3r@gmail.com>
2018-11-02 22:40:53 +0100

    Initial commit

---
+++ src/main/java/pl/mgk/hubertrybarczyk/createyourself/CreateYourselfApplication.java
@@ -0,0 +1,12 @@
+package pl.mgk.hubertrybarczyk.createyourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateYourselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateYourselfApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/mgk/hubertrybarczyk/createyourself/CreateYourselfApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mgk.hubertrybarczyk.createyourself;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreateYourselfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
