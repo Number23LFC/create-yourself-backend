a2790f239b405b5694b7670a85297ef098841912
Number23 <vert3r@gmail.com>
2018-11-13 18:34:14 +0100

    Add Category Entity

---
+++ src/main/java/pl/mgk/hubertrybarczyk/createyourself/model/Category.java
@@ -0,0 +1,49 @@
+package pl.mgk.hubertrybarczyk.createyourself.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    public Category() { // JPA
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
