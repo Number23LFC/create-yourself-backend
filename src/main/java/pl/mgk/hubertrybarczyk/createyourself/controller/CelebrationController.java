c87c7a0539384d204e2afad6cd0711edac70c236
Number23 <vert3r@gmail.com>
2018-11-21 21:54:44 +0100

    Celebration list

--- src/main/java/pl/mgk/hubertrybarczyk/createyourself/controller/CelebrationController.java
+++ src/main/java/pl/mgk/hubertrybarczyk/createyourself/controller/CelebrationController.java
@@ -1,12 +1,16 @@
 package pl.mgk.hubertrybarczyk.createyourself.controller;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.mgk.hubertrybarczyk.createyourself.model.Celebration;
 import pl.mgk.hubertrybarczyk.createyourself.service.CelebrationService;
 
-@Controller
-@RequestMapping("/celebration")
+import java.util.Set;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CelebrationController {
 
     private final CelebrationService celebrationService;
@@ -15,5 +19,11 @@ public class CelebrationController {
         this.celebrationService = celebrationService;
     }
 
+    @GetMapping("/celebrations")
+    public Set<Celebration> findAll() {
+        return celebrationService.findAll();
+    }
+
+    //TODO update save find etc.
 
 }
